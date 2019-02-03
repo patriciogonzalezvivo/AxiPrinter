aff19a3dd005a6c27245f6e0a4d7e21686bcabf5
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2019-02-02 16:07:46 -0800

    big change... choosing file first... plotting later

--- AxiPrinter.py
+++ AxiPrinter.py
@@ -13,26 +13,45 @@ kivy.require('1.0.7')
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
+
 from kivy.properties import ObjectProperty
+from kivy.properties import BooleanProperty
+
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
 from kivy.uix.slider import Slider
+from kivy.uix.button import Button
 
 from pyaxidraw import axidraw
 
-class ModifiedSlider(Slider):
+class MySlider(Slider):
     def __init__(self, **kwargs):
         self.register_event_type('on_release')
-        super(ModifiedSlider, self).__init__(**kwargs)
+        super(MySlider, self).__init__(**kwargs)
 
     def on_release(self):
         pass
 
     def on_touch_up(self, touch):
-        super(ModifiedSlider, self).on_touch_up(touch)
+        super(MySlider, self).on_touch_up(touch)
         if touch.grab_current == self:
             self.dispatch('on_release')
             return True
+
+class MyButton(Button):
+    enabled = BooleanProperty(True)
+
+    def on_enabled(self, instance, value):
+        if value:
+            self.background_color = [1,1,1,1]
+            self.color = [1,1,1,1]
+        else:
+            self.background_color = [1,1,1,.3]
+            self.color = [1,1,1,.5]
+
+    def on_touch_down( self, touch ):
+        if self.enabled:
+            return super(self.__class__, self).on_touch_down(touch)
 
 
 class LoadDialog(FloatLayout):
@@ -44,35 +63,7 @@ class Root(FloatLayout):
     def dismiss_popup(self):
         self._popup.dismiss()
 
-
-    def show_load(self):
-        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Load a SVG file to print", content=content,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
-        
-
-    def load(self, filename):
-        print('Loading', filename[0])
-
-        app = App.get_running_app()
-
-        min_height = app.ad.options.pen_pos_down
-        max_height = app.ad.options.pen_pos_up
-
-        app.ad.plot_setup(filename[0])
-        app.ad.options.model = 2
-        app.ad.options.pen_pos_down = min_height
-        app.ad.options.pen_pos_up = max_height
-        app.ad.plot_run()
-
-        # app.ad.options.preview = True
-        # writeFile = open('current.svg','w')         # Open output file for writing.
-        # writeFile.write( app.ad.plot_run( output=True ) )
-        # writeFile.close()
-
-        self.dismiss_popup()
-
+    # CALIBRATION
     def goCoorner(self, coorner):
         app = App.get_running_app()
 
@@ -87,20 +78,62 @@ class Root(FloatLayout):
         app.ad.penup()
 
         if coorner == 0:
-            ad.goto(app.ad.x_bounds_min, app.ad.y_bounds_min)
+            app.ad.goto(app.ad.x_bounds_min, app.ad.y_bounds_min)
         elif coorner == 1:
-            ad.goto(app.ad.x_bounds_max, app.ad.y_bounds_min)
+            app.ad.goto(app.ad.x_bounds_max, app.ad.y_bounds_min)
         elif coorner == 2:
-            ad.goto(app.ad.x_bounds_min, app.ad.y_bounds_max)
+            app.ad.goto(app.ad.x_bounds_min, app.ad.y_bounds_max)
         elif coorner == 3:
-            ad.goto(app.ad.x_bounds_max, app.ad.y_bounds_max)
+            app.d.goto(app.ad.x_bounds_max, app.ad.y_bounds_max)
 
         app.ad.pendown()
 
+    # FILE
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load a SVG file to print", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+        
 
+    def load(self, filenames):
+        app = App.get_running_app()
+        print('Loading', app.filename)
+
+        min_height = app.ad.options.pen_pos_down
+        max_height = app.ad.options.pen_pos_up
+
+        app.ad.plot_setup(app.filename)
+        app.ad.options.model = 2
+        app.ad.options.pen_pos_down = min_height
+        app.ad.options.pen_pos_up = max_height
+
+        # app.ad.plot_run()
+
+        # app.ad.options.preview = True
+        # writeFile = open('current.svg','w')         # Open output file for writing.
+        # writeFile.write( app.ad.plot_run( output=True ) )
+        # writeFile.close()
+
+        # buttons = MyButton()
+        print(app.root.ids)
+        app.root.ids['flyover_button'].enabled = True
+        app.root.ids['plot_button'].enabled = True
+
+        self.dismiss_popup()
+
+    def flyover(self):
+        app = App.get_running_app()
+        print('FLY OVER', app.filename)
+    
+    def plot(self):
+        app = App.get_running_app()
+        print('PLOT', app.filename)
+        app.ad.plot_run()
 
 class AxiPrinter(App):
     ad = axidraw.AxiDraw() 
+    filename = "None"
 
     def build(self):
         self.ad.interactive()
